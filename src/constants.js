58381466fbce23ee5af3fa9f6c8a59f416ed6295
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-07-02 17:28:32 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const CHANNEL_NAME = 'your channel name';
+export const OAUTH_TOKEN = 'oauth:<your oauth token>';
+export const BOT_USERNAME = 'your bot name';
+export const BLOCKED_WORDS = [
+  'cats',
+  'dogs',
+]
